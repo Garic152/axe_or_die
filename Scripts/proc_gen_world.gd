e50c151469213e27096b44f4f4d6756ec7d56aba
Garic152 <erictheiler15@gmail.com>
2024-11-19 20:26:03 +0100

    Got first working world generation

--- Scripts/proc_gen_world.gd
+++ Scripts/proc_gen_world.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-@onready var tile_map = $TileMapLayer
+@onready var water_layer = $Water
+@onready var ground_layer= $Ground
+@onready var cliff_layer = $Cliffs
+@onready var props_layer = $Props
+
 @export var noise_height_text: NoiseTexture2D
 
 var noise: Noise
@@ -8,10 +12,12 @@ var noise: Noise
 var width: int = 200
 var height: int = width
 
-var land_source = 0
+var source_id = 0
 var land_atlas = Vector2i(2, 6)
-var water_source = 1
-var water_atlas = Vector2i(9, 7)
+var water_atlas = Vector2i(4, 7)
+
+var ground_tiles_arr = []
+
 
 func _ready():
 	noise = noise_height_text.noise
@@ -23,7 +29,7 @@ func generate_world():
 		for y in range(height):
 			var noise_val = noise.get_noise_2d(x, y)
 			if noise_val > 0.0:
-				tile_map.set_cell(Vector2(x, y), land_source, land_atlas)
-			elif noise_val < 0.0:
-				tile_map.set_cell(Vector2(x, y), water_source, water_atlas)
-				pass
+				ground_tiles_arr.append(Vector2i(x, y))
+			water_layer.set_cell(Vector2(x, y), source_id, water_atlas)
+
+	ground_layer.set_cells_terrain_connect(ground_tiles_arr, 1, 0)

d50b363a52ce2445d052818f3bfbba24ac0c161f
Garic152 <erictheiler15@gmail.com>
2024-12-10 19:55:26 +0100

    Added grass texture generation and added noise textures

--- Scripts/proc_gen_world.gd
+++ Scripts/proc_gen_world.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @onready var water_layer = $Water
-@onready var ground_layer= $Ground
+@onready var ground_layer = $Ground
+@onready var grass_layer = $Grass
 @onready var cliff_layer = $Cliffs
 @onready var props_layer = $Props
 
@@ -23,13 +24,20 @@ func _ready():
 	noise = noise_height_text.noise
 	generate_world()
 
+func choose_vector():
+	var rand = randf()
+	if rand < 0.2:
+		return Vector2i(randi_range(10, 11), randi_range(1, 4))
+	return Vector2i(2, 2)
 
 func generate_world():
 	for x in range(width):
 		for y in range(height):
 			var noise_val = noise.get_noise_2d(x, y)
-			if noise_val > 0.0:
+			if noise_val > 0.1:
 				ground_tiles_arr.append(Vector2i(x, y))
+				if noise_val > 0.3:
+					grass_layer.set_cell(Vector2i(x, y), source_id, choose_vector())
 			water_layer.set_cell(Vector2(x, y), source_id, water_atlas)
 
 	ground_layer.set_cells_terrain_connect(ground_tiles_arr, 1, 0)

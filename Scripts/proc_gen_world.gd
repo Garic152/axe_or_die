4199b9d2caa4b196721c6cf16d41e9fe3bd0751c
Garic152 <erictheiler15@gmail.com>
2024-12-11 12:45:53 +0100

    Added basic terrain generation, collisions missing

--- Scripts/proc_gen_world.gd
+++ Scripts/proc_gen_world.gd
@@ -7,21 +7,24 @@ extends Node2D
 @onready var props_layer = $Props
 
 @export var noise_height_text: NoiseTexture2D
-
+@export var noise_tree_text: NoiseTexture2D
 var noise: Noise
+var tree_noise: Noise
 
-var width: int = 200
+var width: int = 50
 var height: int = width
 
 var source_id = 0
 var land_atlas = Vector2i(2, 6)
 var water_atlas = Vector2i(4, 7)
 
+var palm_tree_atlas_arr = [Vector2i(1, 1), Vector2i(6, 1)]
 var ground_tiles_arr = []
 
 
 func _ready():
 	noise = noise_height_text.noise
+	tree_noise = noise_tree_text.noise
 	generate_world()
 
 func choose_vector():
@@ -34,10 +37,13 @@ func generate_world():
 	for x in range(width):
 		for y in range(height):
 			var noise_val = noise.get_noise_2d(x, y)
-			if noise_val > 0.1:
+			var tree_noise_val = tree_noise.get_noise_2d(x, y)
+			if noise_val > 0.25:
 				ground_tiles_arr.append(Vector2i(x, y))
-				if noise_val > 0.3:
+				if noise_val > 0.35:
 					grass_layer.set_cell(Vector2i(x, y), source_id, choose_vector())
+					if tree_noise_val > 0.4:
+						props_layer.set_cell(Vector2i(x, y), 1, palm_tree_atlas_arr.pick_random())
 			water_layer.set_cell(Vector2(x, y), source_id, water_atlas)
 
 	ground_layer.set_cells_terrain_connect(ground_tiles_arr, 1, 0)
